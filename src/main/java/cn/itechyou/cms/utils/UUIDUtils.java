9692a574cbaf6c920c04808f795d77768b5438e4
TAKETODAY <2228592280@qq.com>
2019-10-24 13:25:06 +0800

    :zap: 优化UUIDUtils

--- src/main/java/cn/itechyou/cms/utils/UUIDUtils.java
+++ src/main/java/cn/itechyou/cms/utils/UUIDUtils.java
@@ -4,28 +4,26 @@ import java.util.Random;
 import java.util.UUID;
 
 public class UUIDUtils {
-	public static String getPrimaryKey() {
-		return UUID.randomUUID().toString().replaceAll("-", "");
-	}
-	
-	/**
+    
+    public static String getPrimaryKey() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    /**
      * java生成随机数字和字母组合
      * 
      * @param length[生成随机数的长度]
-     * 
      */
     public static String getCharAndNumr(int length) {
         String val = "";
         Random random = new Random();
         for (int i = 0; i < length; i++) {
             // 输出字母还是数字
-            String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num";
-            // 字符串
-            if ("char".equalsIgnoreCase(charOrNum)) {
-                // 取得大写字母还是小写字母
+            if (random.nextInt(2) % 2 == 0) { // 字符串
                 int choice = random.nextInt(2) % 2 == 0 ? 65 : 97;
                 val += (char) (choice + random.nextInt(26));
-            } else if ("num".equalsIgnoreCase(charOrNum)) { // 数字
+            }
+            else { //num
                 val += String.valueOf(random.nextInt(10));
             }
         }
