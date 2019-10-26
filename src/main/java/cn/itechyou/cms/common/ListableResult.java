2f8670baee7390f795d614d73d6986562e5a088c
TAKETODAY <2228592280@qq.com>
2019-10-26 22:56:21 +0800

    :zap: 添加Pageable分页

---
+++ src/main/java/cn/itechyou/cms/common/ListableResult.java
@@ -0,0 +1,30 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright © Dreamer CMS 2019 All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.itechyou.cms.common;
+
+import java.util.List;
+
+/**
+ * @author TODAY <br>
+ * 		   2019-10-26 22:32
+ */
+public interface ListableResult<T> extends Result {
+
+    @Override
+    List<T> getData();
+
+}
