85645e369169f95ce753aecf1b920f06af6e38cb
TAKETODAY <2228592280@qq.com>
2019-10-26 22:08:41 +0800

    :zap: 优化配置

--- src/main/java/cn/itechyou/cms/common/WebMvcAutoConfiguration.java
+++ src/main/java/cn/itechyou/cms/common/WebMvcAutoConfiguration.java
@@ -16,9 +16,17 @@
  */
 package cn.itechyou.cms.common;
 
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,16 +34,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import cn.itechyou.cms.interceptor.UserAuthorizationInterceptor;
 
 /**
- * 配置静态资源映射
- * 
- * @author sam
- * @since 2017/7/16
+ * @author TODAY <br>
+ *         2019-10-26 22:04
  */
+//@EnableWebMvc
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcAutoConfiguration implements WebMvcConfigurer,
+        ApplicationContextInitializer<ConfigurableWebApplicationContext> {
 
     @Autowired
     private UserAuthorizationInterceptor userAuthorizationInterceptor;
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        converters.add(new DefaultHttpMessageConverter());
+        converters.add(new StringHttpMessageConverter(Constant.DEFAULT_CHARSET));
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -52,6 +67,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(userAuthorizationInterceptor)
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin/u/**");
+    }
+
+    @Override
+    public void initialize(ConfigurableWebApplicationContext applicationContext) {
+
+        ServletContext servletContext = applicationContext.getServletContext();
+        servletContext.setRequestCharacterEncoding(Constant.DEFAULT_ENCODING);
+        servletContext.setResponseCharacterEncoding(Constant.DEFAULT_ENCODING);
+
     }
 
 }
