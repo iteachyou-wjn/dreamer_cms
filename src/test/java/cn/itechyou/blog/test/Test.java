54df8257368df4aa30d2232ba7e865a7446097ea
王俊南 <153095904@qq.com>
2020-07-28 09:00:05 +0800

    提交修改用户名和密码的测试类

--- src/test/java/cn/itechyou/blog/test/Test.java
+++ src/test/java/cn/itechyou/blog/test/Test.java
@@ -2,19 +2,21 @@ package cn.itechyou.blog.test;
 
 import java.io.IOException;
 
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+
 
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		String sb = "<link type=\"text/css\"rel=\"stylesheet\"href=\"{dreamer-cms:template /}css/base.css\"/>\r\n" + 
-				"<link type=\"text/css\"rel=\"stylesheet\"href=\"{dreamer-cms:template /}css/home.css\"/>\r\n" + 
-				"<script type=\"text/javascript\" src=\"{dreamer-cms:template /}js/common.js\"></script>\r\n" + 
-				"<script type=\"text/javascript\" src=\"{dreamer-cms:template /}js/jquery-1.8.2.min.js\"></script>\r\n" + 
-				"<script type=\"text/javascript\" src=\"{dreamer-cms:template /}js/jquery.superslide.js\"></script>\r\n" + 
-				"<script type=\"text/javascript\" src=\"{dreamer-cms:template /}js/adver.js\"></script>";
-		String replaceAll = sb.replaceAll("\\{dreamer-cms:template[ \\t]*[/]{1}\\}", "aaa");
-		System.out.println(replaceAll);
+		String username = "admin";
+		String password = "admin";
+		
+		ByteSource salt = ByteSource.Util.bytes(username + password);
+		SimpleHash sh = new SimpleHash("MD5", password, salt, 1024);
         
-        
+        System.out.printf("密码：%s",sh.toString());
+        System.out.println();
+        System.out.printf("盐：%s",salt.toString());
 	}
 }
