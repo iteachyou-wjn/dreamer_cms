3c5d139047154a16c3c60797da64e02accbf7c9d
王俊南 <153095904@qq.com>
2022-07-28 16:35:51 +0800

    优化分页标签计算逻辑

--- src/main/java/cc/iteachyou/cms/taglib/tags/PaginationTag.java
+++ src/main/java/cc/iteachyou/cms/taglib/tags/PaginationTag.java
@@ -95,10 +95,10 @@ public class PaginationTag implements IParse {
 		sb.append("<ul class='dreamer-ul'>");
 		//
 		String firstUrl = pageurl.replace("{pageNum}", String.valueOf(1)).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
-		if(pageInfo.getTotal() != 0) {
+		if(pageInfo.getTotal() > 0) {
 			sb.append("<li><a href='"+firstUrl+"' title='首页'><i class='fa fa-angle-double-left'></i></a></li>");
 		}
-		if(pageInfo.getPrePage() != 0) {
+		if(pageInfo.isHasPreviousPage()) {
 			String preUrl = pageurl.replace("{pageNum}", String.valueOf(pageInfo.getPrePage())).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
 			sb.append("<li><a href='"+preUrl+"' title='上一页'><i class='fa fa-angle-left'></i></a></li>");
 		}
@@ -113,12 +113,12 @@ public class PaginationTag implements IParse {
 			sb.append("<li class='"+className+"'><a href='"+pageUrl+"' title='第" + pageNum + "页'>"+pageNum+"</a></li>");
 		}
 		
-		if(pageInfo.getNextPage() != 0) {
+		if(pageInfo.isHasNextPage()) {
 			String nextUrl = pageurl.replace("{pageNum}", String.valueOf(pageInfo.getNextPage())).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
 			sb.append("<li><a href='"+nextUrl+"' title='下一页'><i class='fa fa-angle-right'></i></a></li>");
 		}
 		String lastUrl = pageurl.replace("{pageNum}", String.valueOf(pageInfo.getPages())).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
-		if(pageInfo.getTotal() != 0) {
+		if(pageInfo.getTotal() > 0) {
 			sb.append("<li><a href='"+lastUrl+"' title='尾页'><i class='fa fa-angle-double-right'></i></a></li>");
 		}
 		sb.append("</ul>");
@@ -169,9 +169,11 @@ public class PaginationTag implements IParse {
 		sb.append("<div class='dreamer-pagination'>");
 		sb.append("<ul class='dreamer-ul'>");
 		//
-		String firstUrl = pageurl.replace("{pageNum}", String.valueOf(1)).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
-		sb.append("<li><a href='"+firstUrl+"' title='首页'><i class='fa fa-angle-double-left'></i></a></li>");
-		if(pageInfo.getPrePage() != 0) {
+		if(pageInfo.getTotal() > 0) {
+			String firstUrl = pageurl.replace("{pageNum}", String.valueOf(1)).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
+			sb.append("<li><a href='"+firstUrl+"' title='首页'><i class='fa fa-angle-double-left'></i></a></li>");
+		}
+		if(pageInfo.isHasPreviousPage()) {
 			String preUrl = pageurl.replace("{pageNum}", String.valueOf(pageInfo.getPrePage())).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
 			sb.append("<li><a href='"+preUrl+"' title='上一页'><i class='fa fa-angle-left'></i></a></li>");
 		}
@@ -186,12 +188,14 @@ public class PaginationTag implements IParse {
 			sb.append("<li class='"+className+"'><a href='"+pageUrl+"' title='第" + pageNum + "页'>"+pageNum+"</a></li>");
 		}
 		
-		if(pageInfo.getNextPage() != 0) {
+		if(pageInfo.isHasNextPage()) {
 			String nextUrl = pageurl.replace("{pageNum}", String.valueOf(pageInfo.getNextPage())).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
 			sb.append("<li><a href='"+nextUrl+"' title='下一页'><i class='fa fa-angle-right'></i></a></li>");
 		}
-		String lastUrl = pageurl.replace("{pageNum}", String.valueOf(pageInfo.getPages())).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
-		sb.append("<li><a href='"+lastUrl+"' title='尾页'><i class='fa fa-angle-double-right'></i></a></li>");
+		if(pageInfo.getTotal() > 0) {
+			String lastUrl = pageurl.replace("{pageNum}", String.valueOf(pageInfo.getPages())).replace("{pageSize}", String.valueOf(pageInfo.getPageSize()));
+			sb.append("<li><a href='"+lastUrl+"' title='尾页'><i class='fa fa-angle-double-right'></i></a></li>");
+		}
 		sb.append("</ul>");
 		sb.append("</div>");
 		
