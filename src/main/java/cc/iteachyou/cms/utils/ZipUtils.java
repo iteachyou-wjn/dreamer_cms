b1dc636592564df63c80f473330adad4812e2ef6
王俊南 <153095904@qq.com>
2023-02-23 16:40:16 +0800

    修复主题上传时，解压ZIP目录遍历漏洞。

--- src/main/java/cc/iteachyou/cms/utils/ZipUtils.java
+++ src/main/java/cc/iteachyou/cms/utils/ZipUtils.java
@@ -10,14 +10,17 @@ import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import cc.iteachyou.cms.common.ExceptionEnum;
 import cc.iteachyou.cms.entity.Theme;
+import cc.iteachyou.cms.exception.AdminGeneralException;
 
 public class ZipUtils {
 	/**
 	 * 解压文件到指定目录
+	 * @throws AdminGeneralException 
 	 */
 	@SuppressWarnings("rawtypes")
-	public static Theme unZipFiles(File zipFile, String descDir) throws IOException {
+	public static Theme unZipFiles(File zipFile, String descDir) throws IOException, AdminGeneralException {
 		File pathFile = new File(descDir);
 		if (!pathFile.exists()) {
 			pathFile.mkdirs();
@@ -27,9 +30,16 @@ public class ZipUtils {
 		ZipFile zip = new ZipFile(zipFile, Charset.forName("GBK"));
 		for (Enumeration entries = zip.entries(); entries.hasMoreElements();) {
 			ZipEntry entry = (ZipEntry) entries.nextElement();
-			String zipEntryName = entry.getName();
+			String entryName = entry.getName();
+			System.out.println(entryName);
+			if(entryName.contains("../")) {
+				throw new AdminGeneralException(
+						ExceptionEnum.XSS_SQL_EXCEPTION.getCode(),
+						ExceptionEnum.XSS_SQL_EXCEPTION.getMessage(),
+						"压缩包中文件名疑似不安全，详情：" + entryName);
+			}
 			InputStream in = zip.getInputStream(entry);
-			String outPath = (descDir + zipEntryName).replaceAll("\\*", "/");
+			String outPath = (descDir + entryName).replaceAll("\\*", "/");
 			// 判断路径是否存在,不存在则创建文件路径
 			File file = new File(outPath.substring(0, outPath.lastIndexOf('/')));
 			if(file.getParent().equals(pathFile.getAbsolutePath()) && file.isDirectory()) {
@@ -44,8 +54,6 @@ public class ZipUtils {
 				continue;
 			}
 			
-			// 输出文件路径信息
-			System.out.println(outPath);
 			OutputStream out = new FileOutputStream(outPath);
 			byte[] buf1 = new byte[1024];
 			int len;
