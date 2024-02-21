47c4d59d1405bd471248570db17fe17d876a6e44
王俊南 <wang_junnan@hotmail.com>
2024-02-21 14:56:53 +0800

    修复漏洞 https://www.cnvd.org.cn/flaw/show/CNVD-2024-00216

--- src/main/java/cc/iteachyou/cms/interceptor/UserAuthorizationInterceptor.java
+++ src/main/java/cc/iteachyou/cms/interceptor/UserAuthorizationInterceptor.java
@@ -3,7 +3,11 @@ package cc.iteachyou.cms.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cc.iteachyou.cms.entity.System;
+import cc.iteachyou.cms.service.SystemService;
+import cn.hutool.core.util.StrUtil;
 import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -20,7 +24,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class UserAuthorizationInterceptor implements HandlerInterceptor{
-	
+	@Autowired
+	private SystemService systemService;
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -30,19 +35,29 @@ public class UserAuthorizationInterceptor implements HandlerInterceptor{
 			return Boolean.FALSE;
 		}
 
+		System system = systemService.getSystem();
+
 		String referer = request.getHeader("referer");
-		if(referer != null && (referer.startsWith("http://cms.iteachyou.cc/") || referer.startsWith("https://cms.iteachyou.cc/"))) {
-			if(handler instanceof HandlerMethod) {
-				HandlerMethod handlerMethod = (HandlerMethod) handler;
-				Log annotation = handlerMethod.getMethodAnnotation(Log.class);
-				if(annotation == null) {
-					return Boolean.TRUE;
-				}
-				OperatorType operType = annotation.operType();
-				if(!OperatorType.PAGE.equals(operType) && !OperatorType.SELECT.equals(operType) && !OperatorType.OTHER.equals(operType)) {
-					throw new UnauthorizedException("演示环境不允许操作");
+		if(StrUtil.isNotBlank(referer)) {
+			if (!referer.startsWith(system.getWebsite())) {
+				throw new UnauthorizedException("访问来源不合法");
+			}
+
+			if (referer.startsWith("http://cms.iteachyou.cc/") || referer.startsWith("https://cms.iteachyou.cc/")) {
+				if (handler instanceof HandlerMethod) {
+					HandlerMethod handlerMethod = (HandlerMethod) handler;
+					Log annotation = handlerMethod.getMethodAnnotation(Log.class);
+					if (annotation == null) {
+						return Boolean.TRUE;
+					}
+					OperatorType operType = annotation.operType();
+					if (!OperatorType.PAGE.equals(operType) && !OperatorType.SELECT.equals(operType) && !OperatorType.OTHER.equals(operType)) {
+						throw new UnauthorizedException("演示环境不允许操作");
+					}
 				}
 			}
+		}else{
+			throw new UnauthorizedException("访问来源不合法");
 		}
 		log.info("UserAuthorizationInterceptor：["+token+"]拦截通过...");
 		return Boolean.TRUE;
